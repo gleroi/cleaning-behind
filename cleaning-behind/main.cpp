2cee00cc5736e718d001980333104380adaa272a
Guillaume Leroi <leroi.g@gmail.com>
2015-05-22 20:23:52 +0200

    render stain

--- cleaning-behind/main.cpp
+++ cleaning-behind/main.cpp
@@ -1,5 +1,50 @@
 #include <iostream>
 #include <SDL.h>
+#include <vector>
+#include <random>
+
+const int WINDOW_Y_MAX = 640;
+const int WINDOW_X_MAX = 800;
+
+const int TILE_Y_MAX = 25;
+const int TILE_X_MAX = TILE_Y_MAX * 1.33;
+
+enum TileType : int {
+    GROUND = 0,
+    STAIN = 1,
+};
+
+std::vector<TileType>& TILES = *(new std::vector<TileType>(TILE_X_MAX * TILE_Y_MAX));
+
+void InitGame() {
+    for (int y = 0; y < TILE_Y_MAX; y++) {
+        for (int x = 0; x < TILE_X_MAX; x++) {
+            int value = rand() / (RAND_MAX * 1.0f) * 100.f;
+
+            TileType tile = value > 80 ? TileType::STAIN : TileType::GROUND;
+            TILES[y * TILE_Y_MAX + x] = tile;
+        }
+    }
+}
+
+void Render(SDL_Renderer& ren)
+{
+    int tile_h = WINDOW_Y_MAX / TILE_Y_MAX;
+    int tile_w = WINDOW_X_MAX / TILE_X_MAX;
+
+    SDL_Rect rect;
+    rect.h = tile_h;
+    rect.w = tile_w;
+
+    for (int y = 0; y < TILE_Y_MAX; y++) {
+        for (int x = 0; x < TILE_X_MAX; x++) {
+            rect.x = x * tile_w;
+            rect.y = y * tile_h;
+            SDL_SetRenderDrawColor(&ren, TILES[y * TILE_Y_MAX + x] * 255, 0, 0, 0);
+            SDL_RenderFillRect(&ren, &rect);
+        }
+    }
+}
 
 int main(int, char**)
 {
@@ -26,17 +71,27 @@ int main(int, char**)
         return 1;
     }
 
-    // A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
-    for (int i = 0; i < 3; ++i) 
+    srand(5142);
+
+    InitGame();
+
+    bool quit = false;
+    SDL_Event e;
+    //Handle events on queue
+    while (SDL_PollEvent(&e) != 0 || !quit)
     {
+        //User requests quit
+        if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE)
+        {
+            quit = true;
+        }
+
         //First clear the renderer
         SDL_RenderClear(ren);
-        //Draw the texture
-        SDL_SetRenderDrawColor(ren, i * 25, i * 60, i * 12, 0);
-        SDL_RenderFillRect(ren, nullptr);
-        //Update the screen
+
+        Render(*ren);
+
         SDL_RenderPresent(ren);
-        //Take a quick break after all that hard work
-        SDL_Delay(1000);
     }
 }
+
